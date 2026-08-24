e108c7f9b91d4d4cb1a16e25eddbf21f9d9cf3b8
gustavoh-silva <gustavohericksilva@gmail.com>
2026-08-24 00:19:09 -0300

    novos testes e validação no PersistTransactionUseCase

--- 05-spring-ai/src/main/java/dio/budgeting/application/output/TransactionOutput.java
+++ 05-spring-ai/src/main/java/dio/budgeting/application/output/TransactionOutput.java
@@ -3,14 +3,13 @@ package dio.budgeting.application.output;
 import dio.budgeting.domain.Transaction;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 
-public record TransactionOutput(String id, String description, String category, double value) {
+public record TransactionOutput(String id, String description, String category, BigDecimal value) {
     public static TransactionOutput from(Transaction transaction) {
         return new TransactionOutput(
                 transaction.getId().uuid().toString(),
                 transaction.getDescription(),
                 transaction.getCategory().name(),
-                BigDecimal.valueOf(transaction.getAmount()).setScale(2, RoundingMode.HALF_UP).doubleValue());
+                BigDecimal.valueOf(transaction.getAmount(), 2));
     }
 }
